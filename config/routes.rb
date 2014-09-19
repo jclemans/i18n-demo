841dedbfdb1722fce225ae943c387adf223d124c
Jeremy Clemans <jeremy@notch8.com>
2014-09-18 17:48:39 -0700

    localizes model attributes for product descriptions and price

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope "(:locale)" do
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     resources :products
     root to: 'products#index'
   end
